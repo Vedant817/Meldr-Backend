00a0e6de731325b5281f822b85e3dbec8f39569d
Vedant Mahajan <vedantmahajan271@gmail.com>
2024-07-25 00:38:14 +0530

    Cloudinary and Multer configuration

--- controllers/createMedicine.js
+++ controllers/createMedicine.js
@@ -1,4 +1,14 @@
 import { Medicine } from "../model/medicine.model.js";
+import cloudinary from 'cloudinary';
+import multer from "multer";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const upload = multer({ storage: multer.memoryStorage() });
 
 const createMedicine = async (req, res) => {
     try {
