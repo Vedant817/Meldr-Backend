91de8c4c2472db0ac05f709b3372d86276ba521e
Vedant Mahajan <vedantmahajan271@gmail.com>
2024-07-24 22:38:37 +0530

    Filtering of Records

--- controllers/queryMedicine.js
+++ controllers/queryMedicine.js
@@ -3,8 +3,8 @@ import { Medicine } from "../model/medicine.model.js";
 export const searchMedicine = async (req, res) => {
     try {
         const { name } = req.params;
-        const medicine = await Medicine.find({name})
-        if(medicine.length === 0){
+        const medicine = await Medicine.find({ name })
+        if (medicine.length === 0) {
             return res.status(404).json({
                 message: 'Medicine not found'
             });
@@ -15,5 +15,46 @@ export const searchMedicine = async (req, res) => {
         });
     } catch (error) {
         console.log('Error occurred while searching for medicine', error);
+    }
+}
+
+export const filterMedicine = async (req, res) => {
+    try {
+        const { price, discountAvailable, quantityAbove, manufacturer } = req.query;
+
+        if (price) {
+            const abovePrice = await Medicine.find({ price: { $gte: price } });
+            const belowPrice = await Medicine.find({ price: { $lt: price } });
+
+            return res.status(200).json({
+                title1: 'Above Price',
+                abovePrice: abovePrice,
+                title2: 'Below Price',
+                belowPrice: belowPrice
+            });
+        }
+
+        if(discountAvailable){
+            const discountedMedicines = await Medicine.find({discountPrice: {$ne: null}});
+            return res.status(200).json({
+                discountedMedicines
+            });
+        }
+
+        if(quantityAbove){
+            const medicines = await Medicine.find({quantity: {$gte: quantityAbove}});
+            return res.status(200).json({
+                medicines
+            });
+        }
+
+        if(manufacturer){
+            const medicines = await Medicine.find({manufacturer});
+            return res.status(200).json({
+                medicines
+            });
+        }
+    } catch (error) {
+        console.log('Error occurred while filtering medicine', error);
     }
 }
--- routes.js
+++ routes.js
@@ -3,14 +3,15 @@ import createMedicine from './controllers/createMedicine.js'
 import getMedicine from "./controllers/getMedicines.js";
 import updateMedicine from "./controllers/updateMedicine.js";
 import deleteMedicine from "./controllers/deleteMedicine.js";
-import { searchMedicine } from "./controllers/queryMedicine.js";
+import { filterMedicine, searchMedicine } from "./controllers/queryMedicine.js";
 
 const router = Router();
 
 router.route('/create').post(createMedicine);
-router.route('/medicine').get(getMedicine);
+router.route('/all').get(getMedicine);
 router.route('/update/:name').put(updateMedicine);
 router.route('/delete/:name').delete(deleteMedicine);
 router.route('/search/:name').get(searchMedicine);
+router.route('/filter').get(filterMedicine);
 
 export default router;
