d990ebe3e4ba60d8e9b5fc29c91b95ca8ba9920a
Vedant Mahajan <vedantmahajan271@gmail.com>
2024-07-24 15:29:50 +0530

    Updated Schema

--- model/medicine.model.js
+++ model/medicine.model.js
@@ -20,6 +20,15 @@ const medicineSchema = new Schema({
         type: String,
         required: true
     },
+    imageUrl: {
+        type: mongoose.SchemaType.Url,
+        validate: {
+            validator: function (v){
+                return /^https:\/\//.test(v);
+            },
+            message: props => `${props.value} is not a valid URL!`
+        }
+    }
 }
 );
 
